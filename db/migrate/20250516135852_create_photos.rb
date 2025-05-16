627c70a2f89f9ce491da88c84204f39f41fab34d
sapopo888 <sapopo0813@yahoo.co.jp>
2025-05-17 00:27:46 +0900

    マイグレーションファイルにuser追記

--- db/migrate/20250516135852_create_photos.rb
+++ db/migrate/20250516135852_create_photos.rb
@@ -3,6 +3,7 @@ class CreatePhotos < ActiveRecord::Migration[7.2]
     create_table :photos do |t|
       t.json :images
       t.text :comment
+      t.references :user, foreign_key: true
       t.references :album, foreign_key: true
       t.timestamps
     end
