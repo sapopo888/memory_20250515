4280b656c7acebc074e86924ca76846750f4197f
sapopo888 <sapopo0813@yahoo.co.jp>
2025-05-16 21:26:41 +0900

    headerのアルバム作成のリンクにパス追記、アルバム作成機能追加、rails consoleでアルバム作成できること確認

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,4 +1,25 @@
 class AlbumsController < ApplicationController
   def index
+    @albums = current_user.albums.order(created_at: :desc)
+  end
+
+  def new
+    @album = current_user.albums.new
+  end
+
+  def create
+    @album = current_user.albums.new(album_params)
+    if @album.save
+      redirect_to albums_path, status: :see_other, notice: "アルバムを作成しました"
+    else
+      flash.now[:alert] = "アルバムを作成できませんでした"
+    render :new, status: :unprocessable_entity # 作成失敗したエラーメッセージを表示するのに必要
+    end
+  end
+
+  private
+
+  def album_params
+    params.require(:album).permit(:title, :description)
   end
 end
