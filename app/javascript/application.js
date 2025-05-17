ae53c9a6a6541094ea908caa48c65f3b973f8ca0
sapopo888 <sapopo0813@yahoo.co.jp>
2025-05-18 00:32:59 +0900

    yarn add page-flipでインストール、本のように表示成功

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,17 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+import { PageFlip } from "page-flip";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const elm = document.getElementById("book");
+
+  if (elm) {
+    const pageFlip = new PageFlip(document.getElementById('book'), {
+      width: 400,
+      height: 600,
+    });
+
+    pageFlip.loadFromHTML(document.querySelectorAll('.my-page'));
+  }
+});
