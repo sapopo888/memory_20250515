6ba80361a3eb831270c611197cb8d06c51dbadc2
sapopo888 <sapopo0813@yahoo.co.jp>
2025-05-17 09:21:32 +0900

    phhoto.newでエラーメッセージ表示

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,24 +1,30 @@
 class PhotosController < ApplicationController
   def new
-    @album = current_user.album(params[:album_id])
-    @photo = @album.photos.new
+    @album = current_user.albums.find(params[:album_id])
+    @photo = current_user.photos.new
   end
 
   def create
-    @album = current_user.album(params[:album_id])
-    @photo = @album.photos.builds(photo_params)
+    @album = current_user.albums.find(params[:album_id])
+    @photo = current_user.photos.new(photo_params)
+    @photo.album = @album # 関連を設定
 
     if @photo.save
-      redirect_to album_path(params[:album_id]), notice = "画像を保存しました"
+      redirect_to album_path(@album), status: :see_other, notice: "画像を保存しました"
     else
       flash.now[:alert] = "画像を保存できませんでした"
       render :new, status: :unprocessable_entity
     end
   end
 
+  def show
+    @album = current_user.albums.find(params[:album_id])
+    @photos = @album.photos
+  end
+
   private
 
   def photo_params
-    params.require(:photo).permit(:comment, { images: [] } )
+    params.require(:photo).permit(:comment, { images: [] }, :image_cache )
   end
 end
