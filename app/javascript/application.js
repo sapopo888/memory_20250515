1791e2ea72127b726458851a7f4736b5da389e83
sapopo888 <sapopo0813@yahoo.co.jp>
2025-05-18 01:04:43 +0900

    pageflip.js作成して書き出し

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,18 +1,4 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
-import { PageFlip } from "page-flip";
-
-// page-flip
-document.addEventListener("DOMContentLoaded", () => {
-  const elm = document.getElementById("book");
-
-  if (elm) {
-    const pageFlip = new PageFlip(document.getElementById('book'), {
-      width: 400,
-      height: 600,
-    });
-
-    pageFlip.loadFromHTML(document.querySelectorAll('.my-page'));
-  }
-});
+import "./pageflip"
---
+++ app/javascript/pageflip.js
@@ -0,0 +1,14 @@
+import { PageFlip } from "page-flip";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const elm = document.getElementById("book");
+
+  if (elm) {
+    const pageFlip = new PageFlip(document.getElementById('book'), {
+      width: 400,
+      height: 600,
+    });
+
+    pageFlip.loadFromHTML(document.querySelectorAll('.my-page'));
+  }
+});
