9539e0a73d39e456f58a445feafff07083027579
sapopo888 <sapopo0813@yahoo.co.jp>
2025-05-18 14:41:12 +0900

    ルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "memories#top"
 
-  resources :albums, only: %i[index new create show] do
+  resources :albums, only: %i[index new create show destroy] do
     resources :photos, only: %i[new create]
   end
 end
