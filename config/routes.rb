1389004e0f1498ca037a6389d7b4eefc14723dec
sapopo888 <sapopo0813@yahoo.co.jp>
2025-05-20 11:25:34 +0900

    ルーティングの追加

--- config/routes.rb
+++ config/routes.rb
@@ -18,5 +18,7 @@ Rails.application.routes.draw do
 
   resources :albums, only: %i[index new create show edit update destroy] do
     resources :photos, only: %i[new create]
+    get 'edit_photos', to: 'photos#edit_multiple', as: :edit_photos
+    patch 'update_photos' to: 'photos#update_multiple', as: :update_photos
   end
 end
