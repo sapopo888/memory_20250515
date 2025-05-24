ee215b62aa23d6c468cd370509356848c3882d8a
sapopo888 <sapopo0813@yahoo.co.jp>
2025-05-25 00:56:28 +0900

    rubocop修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,7 +12,7 @@ CarrierWave.configure do |config|
       provider: "AWS",
       aws_access_key_id: ENV["S3_ACCESS_KEY_ID"],
       aws_secret_access_key: ENV["S3_SECRET_ACCESS_KEY"],
-      region: ENV["S3_REGION"],
+      region: ENV["S3_REGION"]
       # path_style: true
     }
   else
