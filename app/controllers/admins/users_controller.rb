894cfe270aec2f7dd4c5ebb6d588e5ee4e1456cf
sapopo888 <sapopo0813@yahoo.co.jp>
2025-06-02 01:10:53 +0900

    ユーザー一覧画面のレイアウト作成、削除機能実装

--- app/controllers/admins/users_controller.rb
+++ app/controllers/admins/users_controller.rb
@@ -2,4 +2,14 @@ class Admins::UsersController < Admins::BaseController
   def index
     @users = User.all
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    user.destroy!
+    redirect_to admins_users_path, flash[:notice] = 'ユーザーを削除しました', status: :see_other
+  end
 end
