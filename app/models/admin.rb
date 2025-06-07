9c3411f39b9ab5bfd975ed9a6527dc2ba6001ac7
sapopo888 <sapopo0813@yahoo.co.jp>
2025-06-08 02:03:20 +0900

    admin.rbの:confirmable一旦削除

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -2,6 +2,5 @@ class Admin < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :confirmable # メール認証
+         :recoverable, :rememberable, :validatable
 end
